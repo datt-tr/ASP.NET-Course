e658ba170633f140cf7820240614307ca71351c9
Dat Tran <zrauchz@gmail.com>
2025-04-18 16:05:20 +0200

    feat: add StockController

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e26526e307e76e709c5e3112d0d31a721a77971")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79bfa323acf7aad396e5a5e0cef0800bf95e8204")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
