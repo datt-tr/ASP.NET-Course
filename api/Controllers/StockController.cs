ec7999cc3b576bce43076bdbe0fd19f6bfa57a49
Dat Tran <zrauchz@gmail.com>
2025-04-22 10:35:43 +0200

    feat: add Dtos and Mappers

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -18,7 +19,7 @@ namespace api.Controllers
 
         [HttpGet]
         public IActionResult GetAll(){
-            var stocks = _context.Stocks.ToList();
+            var stocks = _context.Stocks.ToList().Select(s => s.ToStockDto());
 
             return Ok(stocks);
         }
@@ -30,7 +31,7 @@ namespace api.Controllers
                 return NotFound();
             }
 
-            return Ok(stock); 
+            return Ok(stock.ToStockDto()); 
         }
 
     }
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79bfa323acf7aad396e5a5e0cef0800bf95e8204")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e658ba170633f140cf7820240614307ca71351c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
