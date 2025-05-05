6c7704e13f8a7a90ea23e9b3cf228fef101729ab
Dat Tran <zrauchz@gmail.com>
2025-05-05 18:49:16 +0200

    feat: add comment UPDATE

--- api/Mappers/CommentMapper.cs
+++ api/Mappers/CommentMapper.cs
@@ -30,5 +30,13 @@ namespace api.Mappers
                 StockId = stockId
             };
         }
+        public static Comment ToCommentFromUpdate (this UpdateCommentRequestDto commentDto)
+        {
+            return new Comment
+            {
+                Title = commentDto.Title,
+                Content = commentDto.Content
+            };
+        }
     }
 }
