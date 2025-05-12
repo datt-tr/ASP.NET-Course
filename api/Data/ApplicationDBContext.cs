52afc48016311c3cafe3a5f6c67b9b097733c7b5
Dat Tran <zrauchz@gmail.com>
2025-05-12 17:14:35 +0200

    feat: setup Many-To-Many

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -9,21 +9,33 @@ using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
-    public class ApplicationDBContext: IdentityDbContext<AppUser>
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
     {
-        public ApplicationDBContext (DbContextOptions dbContextOptions) : base(dbContextOptions) {
+        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
 
         }
 
-        public DbSet<Stock> 
-        Stocks {get; set; }
-        public DbSet<Comment> Comments {get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
 
-      protected override void OnModelCreating(ModelBuilder builder)
-      {
-         base.OnModelCreating(builder);
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
 
-         List<IdentityRole> roles = new List<IdentityRole>{
+            builder.Entity<Portfolio>(x => x.HasKey(p => new { p.AppUserId, p.StockId }));
+            builder.Entity<Portfolio>()
+            .HasOne(u => u.AppUser)
+            .WithMany(u => u.Portfolios)
+            .HasForeignKey(p => p.AppUserId);
+            
+            builder.Entity<Portfolio>()
+            .HasOne(u => u.Stock)
+            .WithMany(u => u.Portfolios)
+            .HasForeignKey(p => p.StockId);
+
+            List<IdentityRole> roles = new List<IdentityRole>{
             new IdentityRole
             {
                 Id = "Admin",
@@ -39,8 +51,8 @@ namespace api.Data
                 ConcurrencyStamp = "9cf6f117-3f6a-4eb8-bb1a-6b6de2f0dc4c"
             }
          };
-         builder.Entity<IdentityRole>().HasData(roles);
-         
-      }
+            builder.Entity<IdentityRole>().HasData(roles);
+
+        }
     }
 }
